72dab0b7eaee9bbf1ac2b5d566f4bc1ec7927bb0
Gabino Hernandez <gabino.hern.g@gmail.com>
2024-07-04 13:43:29 -0600

    Config can add numbers to string, functionality not finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,41 @@
+import { useState } from "react";
 import Button from "./components/Button";
 import Celda from "./components/Celda";
 
 const App = () => {
+  let operator = null
+
+  const [ mess, setConcatMess] = useState('0.');
+  const buttonClick = (val) => {
+    setConcatMess(mess + val);
+  }
+  const handleOperator = (val) => {
+
+  }
+
   return (
     <>
       <div className="container">
         <div className="block">
-          <Celda>Gruñido</Celda>
+          <Celda>{mess}</Celda>
         </div>
         <div className="containerContent">
-          <Button onClick={() => console.log("Hola")}>1</Button>
-          <Button onClick={() => console.log("Hola")}>2</Button>
-          <Button onClick={() => console.log("Hola")}>3</Button>
-          <Button onClick={() => console.log("Hola")}>+</Button>
-          <Button onClick={() => console.log("Hola")}>4</Button>
-          <Button onClick={() => console.log("Hola")}>5</Button>
-          <Button onClick={() => console.log("Hola")}>6</Button>
-          <Button onClick={() => console.log("Hola")}>-</Button>
-          <Button onClick={() => console.log("Hola")}>7</Button>
-          <Button onClick={() => console.log("Hola")}>8</Button>
-          <Button onClick={() => console.log("Hola")}>9</Button>
-          <Button onClick={() => console.log("Hola")}>/</Button>
-          <Button onClick={() => console.log("Hola")}>.</Button>
-          <Button onClick={() => console.log("Hola")}>0</Button>
-          <Button onClick={() => console.log("Hola")}>*</Button>
-          <Button onClick={() => console.log("Hola")}>=</Button>
+          <Button onClick={() => buttonClick("1")}>1</Button>
+          <Button onClick={() => buttonClick("2")}>2</Button>
+          <Button onClick={() => buttonClick("3")}>3</Button>
+          <Button onClick={() => handleOperator("+")}>+</Button>
+          <Button onClick={() => buttonClick("4")}>4</Button>
+          <Button onClick={() => buttonClick("5")}>5</Button>
+          <Button onClick={() => buttonClick("6")}>6</Button>
+          <Button onClick={() => handleOperator("-")}>-</Button>
+          <Button onClick={() => buttonClick("7")}>7</Button>
+          <Button onClick={() => buttonClick("8")}>8</Button>
+          <Button onClick={() => buttonClick("9")}>9</Button>
+          <Button onClick={() => handleOperator("/")}>/</Button>
+          <Button onClick={() => buttonClick(".")}>.</Button>
+          <Button onClick={() => buttonClick("0")}>0</Button>
+          <Button onClick={() => handleOperator("*")}>*</Button>
+          <Button onClick={() => handleOperator("=")}>=</Button>
 
         </div>
       </div>
